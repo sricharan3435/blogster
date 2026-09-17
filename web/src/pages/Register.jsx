eff37ee1c385c6860dd56806eee0702ee022a340
Sri Charan <sricharan3435@gmail.com>
2026-09-18 00:05:49 +0530

    Add user registration form and API integration

--- web/src/pages/Register.jsx
+++ web/src/pages/Register.jsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+
 function Register() {
-    return <h2>Register Page</h2>;
+
+    const[name, setName] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await fetch(
+            "https://api.sricharan3435.workers.dev/register",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    password: password,
+                }),
+            }
+        );
+        const data = await response.json();
+        console.log(data);
+    }
+
+    return (
+        <div className="auth-page">
+            <h2>Register</h2>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <input 
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button type="submit">Register</button>            
+            </form>
+        </div>
+    );
 }
 
 export default Register;
